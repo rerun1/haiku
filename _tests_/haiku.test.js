eb64431f19eabc5c6ae9aa462cafd910ee1412af
Suzi Rubino <suzi@suzirubi.com>
2020-02-03 23:32:42 -0800

    all testing done on business logic, all three lines test correcty for 6 haikus each

--- _tests_/haiku.test.js
+++ _tests_/haiku.test.js
@@ -51,18 +51,12 @@ describe('Haiku', () => {
     haiku4.getSilentE();
     haiku5.getSilentE();
     haiku6.getSilentE();
-    let silentETest = haiku.silentE;
-    let silentETest2 = haiku2.silentE;
-    let silentETest3 = haiku3.silentE;
-    let silentETest4 = haiku4.silentE;
-    let silentETest5 = haiku5.silentE;
-    let silentETest6 = haiku6.silentE;
-    expect(silentETest).toEqual(1);
-    expect(silentETest2).toEqual(1);
-    expect(silentETest3).toEqual(0);
-    expect(silentETest4).toEqual(2);
-    expect(silentETest5).toEqual(0);
-    expect(silentETest6).toEqual(1);
+    expect(haiku.silentE).toEqual(1);
+    expect(haiku2.silentE).toEqual(1);
+    expect(haiku3.silentE).toEqual(0);
+    expect(haiku4.silentE).toEqual(2);
+    expect(haiku5.silentE).toEqual(0);
+    expect(haiku6.silentE).toEqual(1);
   });
   test('Correctly identify words in the line1 array that have a two vowel string - also tests for 2 or more consecutive vowels', ()=> {
     let haiku = new Haiku("I close my eyes and",line2,line3);
@@ -77,18 +71,12 @@ describe('Haiku', () => {
     haiku4.getDoubleVowel();
     haiku5.getDoubleVowel();
     haiku6.getDoubleVowel();
-    let doubleVowelWordTest = haiku.doubleVowel;
-    let doubleVowelWordTest2 = haiku2.doubleVowel;
-    let doubleVowelWordTest3 = haiku3.doubleVowel;
-    let doubleVowelWordTest4 = haiku4.doubleVowel;
-    let doubleVowelWordTest5 = haiku5.doubleVowel;
-    let doubleVowelWordTest6 = haiku6.doubleVowel;
-    expect(doubleVowelWordTest).toEqual(1);
-    expect(doubleVowelWordTest2).toEqual(2);
-    expect(doubleVowelWordTest3).toEqual(0);
-    expect(doubleVowelWordTest4).toEqual(1);
-    expect(doubleVowelWordTest5).toEqual(4);
-    expect(doubleVowelWordTest6).toEqual(2);
+    expect(haiku.doubleVowel).toEqual(1);
+    expect(haiku2.doubleVowel).toEqual(2);
+    expect(haiku3.doubleVowel).toEqual(0);
+    expect(haiku4.doubleVowel).toEqual(1);
+    expect(haiku5.doubleVowel).toEqual(4);
+    expect(haiku6.doubleVowel).toEqual(2);
   });
   test('Correctly identify word with ending es', ()=> {
     let haiku = new Haiku("I close my eyes and",line2,line3);
@@ -103,18 +91,12 @@ describe('Haiku', () => {
     haiku4.getLastTwoCharEs();
     haiku5.getLastTwoCharEs();
     haiku6.getLastTwoCharEs();
-    let wordTest = haiku.lastTwoCharEs;
-    let wordTest2 = haiku2.lastTwoCharEs;
-    let wordTest3 = haiku3.lastTwoCharEs;
-    let wordTest4 = haiku4.lastTwoCharEs;
-    let wordTest5 = haiku5.lastTwoCharEs;
-    let wordTest6 = haiku6.lastTwoCharEs;
-    expect(wordTest).toEqual(1);
-    expect(wordTest2).toEqual(0);
-    expect(wordTest3).toEqual(0);
-    expect(wordTest4).toEqual(1);
-    expect(wordTest5).toEqual(3);
-    expect(wordTest6).toEqual(0);
+    expect(haiku.lastTwoCharEs).toEqual(1);
+    expect(haiku2.lastTwoCharEs).toEqual(0);
+    expect(haiku3.lastTwoCharEs).toEqual(0);
+    expect(haiku4.lastTwoCharEs).toEqual(1);
+    expect(haiku5.lastTwoCharEs).toEqual(3);
+    expect(haiku6.lastTwoCharEs).toEqual(0);
   });
   test('Correctly break compound words', ()=> {
     let haiku = new Haiku("I close my eyes and",line2,line3);
@@ -129,18 +111,12 @@ describe('Haiku', () => {
     haiku4.getSilentCompounds();
     haiku5.getSilentCompounds();
     haiku6.getSilentCompounds();
-    let compoundFix = haiku.silentCompound;
-    let compoundFix2 = haiku2.silentCompound;
-    let compoundFix3 = haiku3.silentCompound;
-    let compoundFix4 = haiku4.silentCompound;
-    let compoundFix5 = haiku5.silentCompound;
-    let compoundFix6 = haiku6.silentCompound;
-    expect(compoundFix).toEqual(0);
-    expect(compoundFix2).toEqual(0);
-    expect(compoundFix3).toEqual(0);
-    expect(compoundFix4).toEqual(1);
-    expect(compoundFix5).toEqual(0);
-    expect(compoundFix6).toEqual(5);
+    expect(haiku.silentCompound).toEqual(0);
+    expect(haiku2.silentCompound).toEqual(0);
+    expect(haiku3.silentCompound).toEqual(0);
+    expect(haiku4.silentCompound).toEqual(1);
+    expect(haiku5.silentCompound).toEqual(0);
+    expect(haiku6.silentCompound).toEqual(5);
   });
   test('Correctly tally the vowel exceptions on a line of a haiku object', ()=> {
     let haiku = new Haiku("I close my eyes and",line2,line3);
@@ -155,18 +131,12 @@ describe('Haiku', () => {
     haiku4.getVowelExceptions();
     haiku5.getVowelExceptions();
     haiku6.getVowelExceptions();
-    let checkExceptions = haiku.vowelExceptions;
-    let checkExceptions2 = haiku2.vowelExceptions;
-    let checkExceptions3 = haiku3.vowelExceptions;
-    let checkExceptions4 = haiku4.vowelExceptions;
-    let checkExceptions5 = haiku5.vowelExceptions;
-    let checkExceptions6 = haiku6.vowelExceptions;
-    expect(checkExceptions).toEqual(3);
-    expect(checkExceptions2).toEqual(3);
-    expect(checkExceptions3).toEqual(0);
-    expect(checkExceptions4).toEqual(5);
-    expect(checkExceptions5).toEqual(7);
-    expect(checkExceptions6).toEqual(8);
+    expect(haiku.vowelExceptions).toEqual(3);
+    expect(haiku2.vowelExceptions).toEqual(3);
+    expect(haiku3.vowelExceptions).toEqual(0);
+    expect(haiku4.vowelExceptions).toEqual(5);
+    expect(haiku5.vowelExceptions).toEqual(7);
+    expect(haiku6.vowelExceptions).toEqual(8);
   });
   test('Correctly display the number of vowels in vowelCount property', ()=> {
     let haiku = new Haiku(line1,line2,line3);
@@ -187,5 +157,172 @@ describe('Haiku', () => {
     expect(haiku4.vowelCount).toEqual(12);
     expect(haiku5.vowelCount).toEqual(14);
     expect(haiku6.vowelCount).toEqual(19);
+  });
+  test('Correctly display the number of syllables in line1 of the haiku object', ()=> {
+    let haiku = new Haiku(line1,line2,line3);
+    let haiku2 = new Haiku("Haikus are easy","But sometimes they don't make sense","Refrigerator");
+    let haiku3 = new Haiku("x xxxx xxx xxxx xxxx",line2,line3);
+    let haiku4 = new Haiku("But sometimes they don't make sense","Haikus are easy","Arrrg greee moooat beu jess");
+    let haiku5 = new Haiku("monkeys with eyes say yes aboves",line2,line3);
+    let haiku6 = new Haiku("carefree driveway lifeboat stagecraft widebody",line2,line3);
+    haiku.getVowels();
+    haiku.getVowelExceptions();
+    haiku.getSyllables();
+    haiku2.getVowels();
+    haiku2.getVowelExceptions();
+    haiku2.getSyllables();
+    haiku3.getVowels();
+    haiku3.getVowelExceptions();
+    haiku3.getSyllables();
+    haiku4.getVowels();
+    haiku4.getVowelExceptions();
+    haiku4.getSyllables();
+    haiku5.getVowels();
+    haiku5.getVowelExceptions();
+    haiku5.getSyllables();
+    haiku6.getVowels();
+    haiku6.getVowelExceptions();
+    haiku6.getSyllables();
+    expect(haiku.syllables).toEqual(5);
+    expect(haiku2.syllables).toEqual(5);
+    expect(haiku3.syllables).toEqual(0);
+    expect(haiku4.syllables).toEqual(7);
+    expect(haiku5.syllables).toEqual(7);
+    expect(haiku6.syllables).toEqual(11);
+  });
+  test('Correctly display and test for right amount of syllables line1',()=>{
+    let haiku = new Haiku("I close my eyes and",line2,line3);
+    let haiku2 = new Haiku("Haikus are easy","But sometimes they don't make sense","Refrigerator");
+    let haiku3 = new Haiku("x xxxx xxx xxxx xxxx",line2,line3);
+    let haiku4 = new Haiku("But sometimes they don't make sense","Haikus are easy","Arrrg greee moooat beu jess");
+    let haiku5 = new Haiku("monkeys with eyes say yes aboves",line2,line3);
+    let haiku6 = new Haiku("carefree driveway lifeboat stagecraft widebody",line2,line3);
+    haiku.getLineSyllables(haiku.line1);
+
+    expect(haiku.lineArray).toEqual(["I","close","my","eyes","and"]);
+    haiku2.getLineSyllables(haiku2.line1);
+    haiku3.getLineSyllables(haiku3.line1);
+    haiku4.getLineSyllables(haiku4.line1);
+    haiku5.getLineSyllables(haiku5.line1);
+    haiku6.getLineSyllables(haiku6.line1);
+
+    expect(haiku.vowelCount).toEqual(8);
+    expect(haiku.vowelExceptions).toEqual(3);
+    expect(haiku.syllables).toEqual(5);
+    expect(haiku.firstLineSyllableTest).toEqual(true);
+
+    expect(haiku2.vowelCount).toEqual(8);
+    expect(haiku2.vowelExceptions).toEqual(3);
+    expect(haiku2.syllables).toEqual(5);
+    expect(haiku2.firstLineSyllableTest).toEqual(true);
+
+    expect(haiku3.vowelCount).toEqual(0);
+    expect(haiku3.vowelExceptions).toEqual(0);
+    expect(haiku3.syllables).toEqual(0);
+    expect(haiku3.firstLineSyllableTest).toEqual(false);
+
+    expect(haiku4.vowelCount).toEqual(12);
+    expect(haiku4.vowelExceptions).toEqual(5);
+    expect(haiku4.syllables).toEqual(7);
+    expect(haiku4.firstLineSyllableTest).toEqual(false);
+
+    expect(haiku5.vowelCount).toEqual(14);
+    expect(haiku5.vowelExceptions).toEqual(7);
+    expect(haiku5.syllables).toEqual(7);
+    expect(haiku5.firstLineSyllableTest).toEqual(false);
+
+    expect(haiku6.vowelCount).toEqual(19);
+    expect(haiku6.vowelExceptions).toEqual(8);
+    expect(haiku6.syllables).toEqual(11);
+    expect(haiku6.firstLineSyllableTest).toEqual(false);
+  });
+  test('Correctly display and test for right amount of syllables line2',()=>{
+    let haiku = new Haiku("I close my eyes and","try to imagine all the","impossible things");
+    let haiku2 = new Haiku("Haikus are easy","But sometimes they don't make sense","Refrigerator");
+    let haiku3 = new Haiku("x xxxx xxx xxxx xxxx","xxxx xxxx xxx","xxxx xxx xxxx");
+    let haiku4 = new Haiku("But sometimes they don't make sense","Haikus are easy","Arrrg greee moooat beu jess");
+    let haiku5 = new Haiku("monkeys with eyes say yes aboves","try to imagine all the","impossible things");
+    let haiku6 = new Haiku("carefree driveway lifeboat stagecraft widebody","try to imagine all the","impossible things");
+    haiku.getLineSyllables(haiku.line2);
+    haiku2.getLineSyllables(haiku2.line2);
+    haiku3.getLineSyllables(haiku3.line2);
+    haiku4.getLineSyllables(haiku4.line2);
+    haiku5.getLineSyllables(haiku5.line2);
+    haiku6.getLineSyllables(haiku6.line2);
+
+    expect(haiku.vowelCount).toEqual(8);
+    expect(haiku.vowelExceptions).toEqual(1);
+    expect(haiku.syllables).toEqual(7);
+    expect(haiku.secondLineSyllableTest).toEqual(true);
+
+    expect(haiku2.vowelCount).toEqual(12);
+    expect(haiku2.vowelExceptions).toEqual(5);
+    expect(haiku2.syllables).toEqual(7);
+    expect(haiku2.secondLineSyllableTest).toEqual(true);
+
+    expect(haiku3.vowelCount).toEqual(0);
+    expect(haiku3.vowelExceptions).toEqual(0);
+    expect(haiku3.syllables).toEqual(0);
+    expect(haiku3.secondLineSyllableTest).toEqual(false);
+
+    expect(haiku4.vowelCount).toEqual(8);
+    expect(haiku4.vowelExceptions).toEqual(3);
+    expect(haiku4.syllables).toEqual(5);
+    expect(haiku4.secondLineSyllableTest).toEqual(false);
+
+    expect(haiku5.vowelCount).toEqual(8);
+    expect(haiku5.vowelExceptions).toEqual(1);
+    expect(haiku5.syllables).toEqual(7);
+    expect(haiku5.secondLineSyllableTest).toEqual(true);
+
+    expect(haiku6.vowelCount).toEqual(8);
+    expect(haiku6.vowelExceptions).toEqual(1);
+    expect(haiku6.syllables).toEqual(7);
+    expect(haiku6.secondLineSyllableTest).toEqual(true);
+  });
+
+  test('Correctly display and test for right amount of syllables line3',()=>{
+    let haiku = new Haiku("I close my eyes and","try to imagine all the","impossible things");
+    let haiku2 = new Haiku("Haikus are easy","But sometimes they don't make sense","Refrigerator");
+    let haiku3 = new Haiku("x xxxx xxx xxxx xxxx","xxxx xxxx xxx","xxxx xxx xxxx");
+    let haiku4 = new Haiku("But sometimes they don't make sense","Haikus are easy","Arrrg greee moooat beau jess");
+    let haiku5 = new Haiku("monkeys with eyes say yes aboves","try to imagine all the","impossible things");
+    let haiku6 = new Haiku("carefree driveway lifeboat stagecraft widebody","try to imagine all the","impossible things");
+    haiku.getLineSyllables(haiku.line3);
+    haiku2.getLineSyllables(haiku2.line3);
+    haiku3.getLineSyllables(haiku3.line3);
+    haiku4.getLineSyllables(haiku4.line3);
+    haiku5.getLineSyllables(haiku5.line3);
+    haiku6.getLineSyllables(haiku6.line3);
+
+    expect(haiku.vowelCount).toEqual(5);
+    expect(haiku.vowelExceptions).toEqual(0);
+    expect(haiku.syllables).toEqual(5);
+    expect(haiku.thirdLineSyllableTest).toEqual(true);
+
+    expect(haiku2.vowelCount).toEqual(5);
+    expect(haiku2.vowelExceptions).toEqual(0);
+    expect(haiku2.syllables).toEqual(5);
+    expect(haiku2.thirdLineSyllableTest).toEqual(true);
+
+    expect(haiku3.vowelCount).toEqual(0);
+    expect(haiku3.vowelExceptions).toEqual(0);
+    expect(haiku3.syllables).toEqual(0);
+    expect(haiku3.thirdLineSyllableTest).toEqual(false);
+
+    expect(haiku4.vowelCount).toEqual(12);
+    expect(haiku4.vowelExceptions).toEqual(3);
+    expect(haiku4.syllables).toEqual(9);
+    expect(haiku4.thirdLineSyllableTest).toEqual(false);
+
+    expect(haiku5.vowelCount).toEqual(5);
+    expect(haiku5.vowelExceptions).toEqual(0);
+    expect(haiku5.syllables).toEqual(5);
+    expect(haiku5.thirdLineSyllableTest).toEqual(true);
+
+    expect(haiku6.vowelCount).toEqual(5);
+    expect(haiku6.vowelExceptions).toEqual(0);
+    expect(haiku6.syllables).toEqual(5);
+    expect(haiku6.thirdLineSyllableTest).toEqual(true);
   });
 });
--- src/haiku.js
+++ src/haiku.js
@@ -4,12 +4,17 @@ export class Haiku {
     this.line2 = line2;
     this.line3 = line3;
     this.line1Array = this.line1.split(" ");
+    this.lineArray = [];
     this.vowelCount = 0;
     this.silentE = 0;
     this.doubleVowel = 0;
     this.lastTwoCharEs = 0;
     this.silentCompound = 0;
     this.vowelExceptions = 0;
+    this.syllables = 0;
+    this.firstLineSyllableTest = false;
+    this.secondLineSyllableTest = false;
+    this.thirdLineSyllableTest = false;
   }
 
   getSilentE() {
@@ -89,7 +94,55 @@ export class Haiku {
       if(regEx4.test(word)){
         this.silentCompound += 1;
       }
-      this.vowelExceptions = (this.silentE+this.doubleVowel+this.lastTwoCharEs+this.silentCompound);
+    }
+    this.vowelExceptions = (this.silentE+this.doubleVowel+this.lastTwoCharEs+this.silentCompound);
+  }
+  getSyllables(){
+    this.syllables = (this.vowelCount-this.vowelExceptions);
+  }
+  getLineSyllables(string){
+    let vowels = string.match(/[aeiouy]/gi);
+    if (vowels == null) {
+      this.vowelCount = 0;
+    } else {
+      this.vowelCount = vowels.length;
+    }
+    this.lineArray = string.split(" ");
+    // silent last vowel
+    for(let i = 0; i < this.lineArray.length; i++) {
+      let word = this.lineArray[i];
+      let lastChar = word.charAt(word.length-1);
+      let lastTwoChar = word.slice(word.length-2);
+      let regEx = /e/gi;
+      if (regEx.test(lastChar) && word.length > 2&&word!="the"&&lastTwoChar!="le"){
+        this.silentE += 1;
+      }
+      // double vowel
+      let regEx2 = /[aeiouy]+[aeiouy]+/gi;
+      let checkSlice1 = word.slice(word.length-1);
+      if (regEx2.test(word)&&checkSlice1!="e"){
+        this.doubleVowel += 1;
+      }
+      // last two characters es
+      let checkSlice3 = word.slice(word.length-3);
+      let checkSlice2 = word.slice(word.length-2);
+      let regEx3 = /[a-z]es/gi;
+      if (regEx3.test(checkSlice3)&&checkSlice2!="yes") {
+        this.lastTwoCharEs += 1;
+      }
+      // silent vowel in compound
+      let regEx4 = /above[^ ].|care[^ ].|cave[^ ].|cheese[^ ].|driveway|else[^ ].|eye[^ ].|fire[^ ].|fore[^ ].|here[^ ].|home[^ ].|horse[^ ].|house[^ ].|life[^ ].|noise[^ ].|sauce[^ ].|shore[^ ].|some[^ ].|same[^ ].|skate[^ ].|stage[^ ].|take[^ ].|time[^ ].|type[^ ].|ware[^ ].|wave[^ ].|waste[^ ].|wide[^ ].|wipe[^ ]./gi;
+      if(regEx4.test(word)){
+        this.silentCompound += 1;
+      }
+    }
+    this.vowelExceptions = (this.silentE+this.doubleVowel+this.lastTwoCharEs+this.silentCompound);
+    this.syllables = (this.vowelCount-this.vowelExceptions);
+    if ((string == this.line1 || string == this.line3)&&(this.syllables == 5)) {
+      this.firstLineSyllableTest = true;
+      this.thirdLineSyllableTest = true;
+    } else if (string == this.line2&&this.syllables == 7) {
+      this.secondLineSyllableTest = true;
     }
   }
 
