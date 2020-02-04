a20c331a72ccd5a3a3ceefa9191d881a13d59a0d
Suzi Rubino <suzi@suzirubi.com>
2020-02-04 12:55:39 -0800

    application works as expected

--- src/haiku.js
+++ src/haiku.js
@@ -16,90 +16,14 @@ export class Haiku {
     this.thirdLineSyllableTest = false;
   }
 
-  getSilentE() {
-    for(let i = 0; i < this.line1Array.length; i++) {
-      let word = this.line1Array[i];
-      let lastChar = word.charAt(word.length-1);
-      let regEx = /e/i;
-      if (regEx.test(lastChar) && word.length > 2){
-        this.silentE += 1;
-      }
-    }
-  }
-
-  getDoubleVowel() {
-    for(let i = 0; i < this.line1Array.length; i++) {
-      let word = this.line1Array[i];
-      let regEx = /[aeiouy]+[aeiouy]+/gi;
-      let checkSlice1 = word.slice(word.length-1);
-      if (regEx.test(word)&&checkSlice1!="e"){
-        this.doubleVowel += 1;
-      }
-    }
-  }
-  getLastTwoCharEs() {
-    for (let i = 0; i < this.line1Array.length; i ++) {
-      let word = this.line1Array[i];
-      let checkSlice3 = word.slice(word.length-3);
-      let checkSlice2 = word.slice(word.length-2);
-      let regEx = /[a-z]es/gi;
-      if (regEx.test(checkSlice3)&&checkSlice2!="yes") {
-        this.lastTwoCharEs += 1;
-      }
-    }
-  }
-  getSilentCompounds(){
-    for (let i = 0; i < this.line1Array.length; i ++) {
-      let word = this.line1Array[i];
-      let regEx = /above[^ ].|care[^ ].|cave[^ ].|cheese[^ ].|driveway|else[^ ].|eye[^ ].|fire[^ ].|fore[^ ].|here[^ ].|home[^ ].|horse[^ ].|house[^ ].|life[^ ].|noise[^ ].|sauce[^ ].|shore[^ ].|some[^ ].|same[^ ].|skate[^ ].|stage[^ ].|take[^ ].|time[^ ].|type[^ ].|ware[^ ].|wave[^ ].|waste[^ ].|wide[^ ].|wipe[^ ]./gi;
-      if(regEx.test(word)){
-        this.silentCompound += 1;
-      }
-    }
-  }
-  getVowels(){
-    let vowels = this.line1.match(/[aeiouy]/gi);
-    if (vowels == null) {
-      this.vowelCount = 0;
-    } else {
-      this.vowelCount = vowels.length;
-    }
-  }
-
-  getVowelExceptions(){
-    // silent last vowel
-    for(let i = 0; i < this.line1Array.length; i++) {
-      let word = this.line1Array[i];
-      let lastChar = word.charAt(word.length-1);
-      let regEx = /e/i;
-      if (regEx.test(lastChar) && word.length > 2){
-        this.silentE += 1;
-      }
-      // double vowel
-      let regEx2 = /[aeiouy]+[aeiouy]+/gi;
-      let checkSlice1 = word.slice(word.length-1);
-      if (regEx2.test(word)&&checkSlice1!="e"){
-        this.doubleVowel += 1;
-      }
-      // last two characters es
-      let checkSlice3 = word.slice(word.length-3);
-      let checkSlice2 = word.slice(word.length-2);
-      let regEx3 = /[a-z]es/gi;
-      if (regEx3.test(checkSlice3)&&checkSlice2!="yes") {
-        this.lastTwoCharEs += 1;
-      }
-      // silent vowel in compound
-      let regEx4 = /above[^ ].|care[^ ].|cave[^ ].|cheese[^ ].|driveway|else[^ ].|eye[^ ].|fire[^ ].|fore[^ ].|here[^ ].|home[^ ].|horse[^ ].|house[^ ].|life[^ ].|noise[^ ].|sauce[^ ].|shore[^ ].|some[^ ].|same[^ ].|skate[^ ].|stage[^ ].|take[^ ].|time[^ ].|type[^ ].|ware[^ ].|wave[^ ].|waste[^ ].|wide[^ ].|wipe[^ ]./gi;
-      if(regEx4.test(word)){
-        this.silentCompound += 1;
-      }
-    }
-    this.vowelExceptions = (this.silentE+this.doubleVowel+this.lastTwoCharEs+this.silentCompound);
-  }
-  getSyllables(){
-    this.syllables = (this.vowelCount-this.vowelExceptions);
-  }
   getLineSyllables(string){
+    this.vowelCount = 0;
+    this.silentE = 0;
+    this.doubleVowel = 0;
+    this.lastTwoCharEs = 0;
+    this.silentCompound = 0;
+    this.vowelExceptions = 0;
+    this.syllables = 0;
     let vowels = string.match(/[aeiouy]/gi);
     if (vowels == null) {
       this.vowelCount = 0;
@@ -137,14 +61,100 @@ export class Haiku {
     }
     this.vowelExceptions = (this.silentE+this.doubleVowel+this.lastTwoCharEs+this.silentCompound);
     this.syllables = (this.vowelCount-this.vowelExceptions);
-    if ((string == this.line1 || string == this.line3)&&(this.syllables == 5)) {
+    if (string == this.line1 && this.syllables == 5) {
       this.firstLineSyllableTest = true;
-      this.thirdLineSyllableTest = true;
-    } else if (string == this.line2&&this.syllables == 7) {
+    }
+    if (string == this.line2 && this.syllables == 7) {
       this.secondLineSyllableTest = true;
+    }
+    if (string == this.line3 && this.syllables == 5) {
+      this.thirdLineSyllableTest = true;
     }
   }
 
 }
 
-    // this.fullHaiku = `${this.line1} ${this.line2} ${this.line3}`;
+//
+// getSilentE() {
+//   for(let i = 0; i < this.line1Array.length; i++) {
+//     let word = this.line1Array[i];
+//     let lastChar = word.charAt(word.length-1);
+//     let regEx = /e/i;
+//     if (regEx.test(lastChar) && word.length > 2){
+//       this.silentE += 1;
+//     }
+//   }
+// }
+//
+// getDoubleVowel() {
+//   for(let i = 0; i < this.line1Array.length; i++) {
+//     let word = this.line1Array[i];
+//     let regEx = /[aeiouy]+[aeiouy]+/gi;
+//     let checkSlice1 = word.slice(word.length-1);
+//     if (regEx.test(word)&&checkSlice1!="e"){
+//       this.doubleVowel += 1;
+//     }
+//   }
+// }
+// getLastTwoCharEs() {
+//   for (let i = 0; i < this.line1Array.length; i ++) {
+//     let word = this.line1Array[i];
+//     let checkSlice3 = word.slice(word.length-3);
+//     let checkSlice2 = word.slice(word.length-2);
+//     let regEx = /[a-z]es/gi;
+//     if (regEx.test(checkSlice3)&&checkSlice2!="yes") {
+//       this.lastTwoCharEs += 1;
+//     }
+//   }
+// }
+// getSilentCompounds(){
+//   for (let i = 0; i < this.line1Array.length; i ++) {
+//     let word = this.line1Array[i];
+//     let regEx = /above[^ ].|care[^ ].|cave[^ ].|cheese[^ ].|driveway|else[^ ].|eye[^ ].|fire[^ ].|fore[^ ].|here[^ ].|home[^ ].|horse[^ ].|house[^ ].|life[^ ].|noise[^ ].|sauce[^ ].|shore[^ ].|some[^ ].|same[^ ].|skate[^ ].|stage[^ ].|take[^ ].|time[^ ].|type[^ ].|ware[^ ].|wave[^ ].|waste[^ ].|wide[^ ].|wipe[^ ]./gi;
+//     if(regEx.test(word)){
+//       this.silentCompound += 1;
+//     }
+//   }
+// }
+// getVowels(){
+//   let vowels = this.line1.match(/[aeiouy]/gi);
+//   if (vowels == null) {
+//     this.vowelCount = 0;
+//   } else {
+//     this.vowelCount = vowels.length;
+//   }
+// }
+//
+// getVowelExceptions(){
+//   // silent last vowel
+//   for(let i = 0; i < this.line1Array.length; i++) {
+//     let word = this.line1Array[i];
+//     let lastChar = word.charAt(word.length-1);
+//     let regEx = /e/i;
+//     if (regEx.test(lastChar) && word.length > 2){
+//       this.silentE += 1;
+//     }
+//     // double vowel
+//     let regEx2 = /[aeiouy]+[aeiouy]+/gi;
+//     let checkSlice1 = word.slice(word.length-1);
+//     if (regEx2.test(word)&&checkSlice1!="e"){
+//       this.doubleVowel += 1;
+//     }
+//     // last two characters es
+//     let checkSlice3 = word.slice(word.length-3);
+//     let checkSlice2 = word.slice(word.length-2);
+//     let regEx3 = /[a-z]es/gi;
+//     if (regEx3.test(checkSlice3)&&checkSlice2!="yes") {
+//       this.lastTwoCharEs += 1;
+//     }
+//     // silent vowel in compound
+//     let regEx4 = /above[^ ].|care[^ ].|cave[^ ].|cheese[^ ].|driveway|else[^ ].|eye[^ ].|fire[^ ].|fore[^ ].|here[^ ].|home[^ ].|horse[^ ].|house[^ ].|life[^ ].|noise[^ ].|sauce[^ ].|shore[^ ].|some[^ ].|same[^ ].|skate[^ ].|stage[^ ].|take[^ ].|time[^ ].|type[^ ].|ware[^ ].|wave[^ ].|waste[^ ].|wide[^ ].|wipe[^ ]./gi;
+//     if(regEx4.test(word)){
+//       this.silentCompound += 1;
+//     }
+//   }
+//   this.vowelExceptions = (this.silentE+this.doubleVowel+this.lastTwoCharEs+this.silentCompound);
+// }
+// getSyllables(){
+//   this.syllables = (this.vowelCount-this.vowelExceptions);
+// }
--- src/main.js
+++ src/main.js
@@ -4,28 +4,67 @@ import './styles.css';
 import $ from 'jquery';
 import { Haiku } from './haiku.js';
 
+
 function resetForm(){
   $("input#line1").val("");
   $("input#line2").val("");
   $("input#line3").val("");
+  // $("#firstLineRight").empty();
 }
+function clearFields(){
+  $("ul#linesRight").empty();
+  $("ul#linesWrong").empty();
+}
+
 
 $(document).ready(function(){
   $("form#inputLines").submit(function(event){
     event.preventDefault();
 
+    clearFields();
+
     let userLine1 = $("input#line1").val();
     let userLine2 = $("input#line2").val();
     let userLine3 = $("input#line3").val();
     let haiku = new Haiku(userLine1,userLine2,userLine3);
-    let firstLine = haiku.getLineSyllables(haiku.line1);
-    $("p#firstLine").text(userLine1);
-    $("span#firstLineRight").text("Great job! Your first line is "+haiku.)
-    console.log(haiku);
+
+    haiku.getLineSyllables(haiku.line1);
+    haiku.getLineSyllables(haiku.line2);
+    haiku.getLineSyllables(haiku.line3);
+
+    let firstLine = haiku.line1;
+    let secondLine = haiku.line2;
+    let thirdLine = haiku.line3;
+
+    $("#firstLine").text(firstLine);
+    $("#secondLine").text(secondLine);
+    $("#thirdLine").text(thirdLine);
+
+
+    if (haiku.firstLineSyllableTest&&haiku.secondLineSyllableTest&&haiku.thirdLineSyllableTest) {
+      $("ul#linesRight").append("<li>Great job! Your Haiku has a first line of 5 syllables, a second line of 7 syllables and the last line is 5 syllables.</li>");
+    } else if (haiku.firstLineSyllableTest&&haiku.secondLineSyllableTest&&!haiku.thirdLineSyllableTest) {
+      $("ul#linesWrong").append("<li>Interesting poem, but it's not Haiku. The third line does not have exactly 5 syllables.</li>");
+    } else if (haiku.secondLineSyllableTest&&haiku.thirdLineSyllableTest&&!haiku.firstLineSyllableTest) {
+      $("ul#linesWrong").append("<li>Interesting poem, but it's not Haiku. The first line does not have exactly 5 syllables.</li>");
+    } else if (haiku.thirdLineSyllableTest&&haiku.firstLineSyllableTest&&!haiku.secondLineSyllableTest) {
+      $("ul#linesWrong").append("<li>Interesting poem, but it's not Haiku. The second line does not have exactly 7 syllables.</li>");
+    } else if (haiku.firstLineSyllableTest&&!haiku.secondLineSyllableTest&&!haiku.thirdLineSyllableTest) {
+      $("ul#linesWrong").append("<li>Interesting poem, but it's not Haiku. The second line does not have 7 syllables and the third line does not have exactly 5 syllables.</li>");
+    } else if (haiku.secondLineSyllableTest&&!haiku.thirdLineSyllableTest&&!haiku.firstLineSyllableTest) {
+      $("ul#linesWrong").append("<li>Interesting poem, but it's not Haiku. The first and third lines do not have exactly 5 syllables.</li>");
+    } else if (haiku.thirdLineSyllableTest&&!haiku.firstLineSyllableTest&&!haiku.secondLineSyllableTest) {
+      $("ul#linesWrong").append("<li>Interesting poem, but it's not Haiku. The first line does not have exactly 5 syllables and the second line does not have exactly 7 syllables.</li>");
+    } else if (!haiku.thirdLineSyllableTest&&!haiku.firstLineSyllableTest&&!haiku.secondLineSyllableTest) {
+      $("ul#linesWrong").append("<li>Interesting poem, but it's not Haiku. The first line does not have exactly 5 syllables, the second does not have exactly 7 syllables and the third line does not have exactly 5 syllables.</li>");
+    }
 
     resetForm();
   });
 });
+
+
+
 
 // right
 // const line1 = "I close my eyes and";
