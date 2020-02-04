deec06515705b2f527b85db2d241c244d406ee95
Suzi Rubino <suzi@suzirubi.com>
2020-02-03 20:35:54 -0800

    all tests for correct vowel count and vowel exceptions complete

--- src/haiku.js
+++ src/haiku.js
@@ -1,74 +1,98 @@
 export class Haiku {
-  constructor(line1){
+  constructor(line1,line2,line3){
     this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
     this.line1Array = this.line1.split(" ");
-    this.getVowelCount = 0;
-    this.lastCharVowel = [];
-    this.doubleVowel = [];
-    this.lastTwoCharEs = [];
-    this.silentCompound = [];
+    this.vowelCount = 0;
+    this.silentE = 0;
+    this.doubleVowel = 0;
+    this.lastTwoCharEs = 0;
+    this.silentCompound = 0;
+    this.vowelExceptions = 0;
   }
-  getVowels(){
-    let vowels = this.line1.match(/[aeiou]/gi);
-    return vowels;
-  }
-  getLastCharVowel() {
-    for (let i = 0; i < this.line1Array.length; i ++) {
+
+  getSilentE() {
+    for(let i = 0; i < this.line1Array.length; i++) {
       let word = this.line1Array[i];
       let lastChar = word.charAt(word.length-1);
-      let sliceAt = word.length-1;
-      let newWord = word.slice(0,sliceAt);
-      let regEx = /[aeiou]/gi;
-      if (word.length <= 2) {
-        this.lastCharVowel.push(word);
-      } else if (regEx.test(lastChar)) {
-        this.lastCharVowel.push(newWord);
-      } else {
-        this.lastCharVowel.push(word);
+      let regEx = /e/i;
+      if (regEx.test(lastChar) && word.length > 2){
+        this.silentE += 1;
       }
     }
   }
+
   getDoubleVowel() {
-    for (let i = 0; i < this.line1Array.length; i ++) {
+    for(let i = 0; i < this.line1Array.length; i++) {
       let word = this.line1Array[i];
-      let regEx = /[aeiou]+[a+eiou]+/gi;
-      if (word.length <= 2)  {
-        this.doubleVowel.push(word);
-      } else if (regEx.test(word)) {
-        let newWord = word.replace(regEx,"x");
-        this.doubleVowel.push(newWord);
-      } else {
-        this.doubleVowel.push(word);
+      let regEx = /[aeiouy]+[aeiouy]+/gi;
+      let checkSlice1 = word.slice(word.length-1);
+      if (regEx.test(word)&&checkSlice1!="e"){
+        this.doubleVowel += 1;
       }
     }
   }
   getLastTwoCharEs() {
     for (let i = 0; i < this.line1Array.length; i ++) {
       let word = this.line1Array[i];
-      let sliceAt = word.length-2;
-      let checkSlice = word.slice(word.length-2);
-      let newWord = word.slice(0,sliceAt);
-      let regEx = /es/gi;
-      if (word.length <= 3) {
-        this.lastTwoCharEs.push(word);
-      } else if (regEx.test(checkSlice)) {
-        this.lastTwoCharEs.push(newWord);
-      } else {
-        this.lastTwoCharEs.push(word);
+      let checkSlice3 = word.slice(word.length-3);
+      let checkSlice2 = word.slice(word.length-2);
+      let regEx = /[a-z]es/gi;
+      if (regEx.test(checkSlice3)&&checkSlice2!="yes") {
+        this.lastTwoCharEs += 1;
       }
     }
   }
   getSilentCompounds(){
     for (let i = 0; i < this.line1Array.length; i ++) {
       let word = this.line1Array[i];
-      let regEx = /above[^ ].|care[^ ].|cave[^ ].|cheese[^ ].|driveway[^ ].|else[^ ].|eye[^ ].|fire[^ ].|fore[^ ].|here[^ ].|home[^ ].|horse[^ ].|house[^ ].|life[^ ].|noise[^ ].|sauce[^ ].|shore[^ ].|some[^ ].|same[^ ].|skate[^ ].|stage[^ ].|take[^ ].|time[^ ].|type[^ ].|ware[^ ].|wave[^ ].|waste[^ ].|wide[^ ].|wipe[^ ]./gi;
+      let regEx = /above[^ ].|care[^ ].|cave[^ ].|cheese[^ ].|driveway|else[^ ].|eye[^ ].|fire[^ ].|fore[^ ].|here[^ ].|home[^ ].|horse[^ ].|house[^ ].|life[^ ].|noise[^ ].|sauce[^ ].|shore[^ ].|some[^ ].|same[^ ].|skate[^ ].|stage[^ ].|take[^ ].|time[^ ].|type[^ ].|ware[^ ].|wave[^ ].|waste[^ ].|wide[^ ].|wipe[^ ]./gi;
       if(regEx.test(word)){
-        this.silentCompound.push(word+" bingo");
-      } else {
-        this.silentCompound.push(word);
+        this.silentCompound += 1;
       }
     }
   }
+  getVowels(){
+    let vowels = this.line1.match(/[aeiouy]/gi);
+    if (vowels == null) {
+      this.vowelCount = 0;
+    } else {
+      this.vowelCount = vowels.length;
+    }
+  }
+
+  getVowelExceptions(){
+    // silent last vowel
+    for(let i = 0; i < this.line1Array.length; i++) {
+      let word = this.line1Array[i];
+      let lastChar = word.charAt(word.length-1);
+      let regEx = /e/i;
+      if (regEx.test(lastChar) && word.length > 2){
+        this.silentE += 1;
+      }
+      // double vowel
+      let regEx2 = /[aeiouy]+[aeiouy]+/gi;
+      let checkSlice1 = word.slice(word.length-1);
+      if (regEx2.test(word)&&checkSlice1!="e"){
+        this.doubleVowel += 1;
+      }
+      // last two characters es
+      let checkSlice3 = word.slice(word.length-3);
+      let checkSlice2 = word.slice(word.length-2);
+      let regEx3 = /[a-z]es/gi;
+      if (regEx3.test(checkSlice3)&&checkSlice2!="yes") {
+        this.lastTwoCharEs += 1;
+      }
+      // silent vowel in compound
+      let regEx4 = /above[^ ].|care[^ ].|cave[^ ].|cheese[^ ].|driveway|else[^ ].|eye[^ ].|fire[^ ].|fore[^ ].|here[^ ].|home[^ ].|horse[^ ].|house[^ ].|life[^ ].|noise[^ ].|sauce[^ ].|shore[^ ].|some[^ ].|same[^ ].|skate[^ ].|stage[^ ].|take[^ ].|time[^ ].|type[^ ].|ware[^ ].|wave[^ ].|waste[^ ].|wide[^ ].|wipe[^ ]./gi;
+      if(regEx4.test(word)){
+        this.silentCompound += 1;
+      }
+      this.vowelExceptions = (this.silentE+this.doubleVowel+this.lastTwoCharEs+this.silentCompound);
+    }
+  }
+
 }
 
     // this.fullHaiku = `${this.line1} ${this.line2} ${this.line3}`;
